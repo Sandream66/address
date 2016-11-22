9489a318dbd4fd95efc9fabfc6d2378be4ffff47
松本　綾子 <sandream66@gmail.com>
2016-11-22 20:55:00 +0900

    re_allcode

--- allcode.py
+++ allcode.py
@@ -9,37 +9,37 @@ import sys
 
 args = sys.argv
 filename = ""
-#try:
-filename = args[1]
-if filename:
-    f = open(filename, 'r')
-    newcsv = []
-    top_list = [
-            'name',
-            'zip code',
-            'address',
-            'phone',
-            'mobile_pyone'
-                    ]
-    newcsv.append(top_list)
-    for i in csv.DictReader(f):
-        line = []
-        name = " ".join((i['lastname'], i['firstname']))
-        zip_code = ":".join(("郵便番号", i['zip code']))
-        address = "".join((i['address1'], i['address2'], i['address3']))
-        phone = "：".join(("電話番号", i['phone']))
-        mobilephone = "：".join(("携帯番号", i['mobilephone']))
-        line.append(name)
-        line.append(zip_code)
-        line.append(address)
-        line.append(phone)
-        line.append(mobilephone)
-        newcsv.append(line)
+try:
+    filename = args[1]
+    if filename:
+        f = open(filename, 'r')
+        newcsv = []
+        top_list = [
+                'name',
+                'zip code',
+                'address',
+                'phone',
+                'mobile_pyone'
+                        ]
+        newcsv.append(top_list)
+        for i in csv.DictReader(f):
+            line = []
+            name = " ".join((i['lastname'], i['firstname']))
+            zip_code = ":".join(("郵便番号", i['zip code']))
+            address = "".join((i['address1'], i['address2'], i['address3']))
+            phone = "：".join(("電話番号", i['phone']))
+            mobilephone = "：".join(("携帯番号", i['mobilephone']))
+            line.append(name)
+            line.append(zip_code)
+            line.append(address)
+            line.append(phone)
+            line.append(mobilephone)
+            newcsv.append(line)
 
-    f = open("allcode.csv", "w")
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerows(newcsv)
+        f = open("allcode.csv", "w")
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(newcsv)
 
-    f.close()
-#except:
-    #print("no filename")
+        f.close()
+except:
+    print("no filename")
